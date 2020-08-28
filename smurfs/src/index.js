30776ec483c7d865ac2a119db985b1312985a965
EmilyLow <emlow@me.com>
2020-08-28 18:19:37 -0500

    Initial commit

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -4,3 +4,5 @@ import "./index.css";
 import App from "./components/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
+//Is app ready to roll with proper middleware? I don't see any middleware but I'm unclear what should be here. Or if this is the correct index.js
